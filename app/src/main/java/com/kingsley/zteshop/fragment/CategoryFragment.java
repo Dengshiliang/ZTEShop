61f7c31469365132c377344e08b774dceb3e80f9
KingsleyDeng <KingsleyDeng@users.noreply.github.com>
2017-08-22 17:28:11 +0800

    Initial commit

---
+++ app/src/main/java/com/kingsley/zteshop/fragment/CategoryFragment.java
@@ -0,0 +1,29 @@
+package com.kingsley.zteshop.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kingsley.zteshop.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class CategoryFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return  inflater.inflate(R.layout.fragment_category,container,false);
+    }
+}
+
+
+
